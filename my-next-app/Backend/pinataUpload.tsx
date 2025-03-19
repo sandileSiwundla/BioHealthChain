06a6d83a04da59ea544662cbca78c8af2f31ecdf
Sandile <ssiwundla023@student.wethinkcode.co.za>
2025-03-19 06:42:48 +0200

    successfully added file to pinata.

--- my-next-app/Backend/pinataUpload.tsx
+++
@@ -1,9 +0,0 @@
-import { PinataSDK } from "pinata";
-
-const pinata = new PinataSDK({
-  pinataJwt: process.env.PINATA_JWT!,
-  pinataGateway: "example-gateway.mypinata.cloud",
-});
-
-const file = new File(["hello world!"], "hello.txt", { type: "text/plain" })
-const upload = await pinata.upload.file(file)
