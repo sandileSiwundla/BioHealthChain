c216f264c15206bf8bf69b6581e36775316ea561
sandileSiwundla <sandsmthembu2@gmail.com>
2025-09-04 04:02:37 +0200

    Fixing vercel.

---
+++ Backend/scripts/uploadToIpfs.py
@@ -0,0 +1,10 @@
+# import sys
+# import os
+
+# def uploadToIPFS():
+#     os.system('ipfs daemon')
+#     os.wait()
+#     os.system('ipfs add --pin(add file name)')
+#     os.wait()
+#     os.system('ipfs shutdown')
+   
