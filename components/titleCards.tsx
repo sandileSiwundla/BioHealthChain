f836ec67119d0987fa32738368638033ad3760ea
sandileSiwundla <sandsmthembu2@gmail.com>
2025-09-07 21:03:11 +0200

    Added FeatureGrids

--- components/titleCards.tsx
+++ components/titleCards.tsx
@@ -1,22 +1,22 @@
 import React from "react";
 
-
 interface FeatureCardProps {
-  title: string;
-  description?: string;
-  onClick?: () => void;
+    title: string;
+    description?: string;
+    onClick?: () => void;
 }
 
+
 const FeatureCard: React.FC<FeatureCardProps> = ({ title, description, onClick }) => {
-  return (
-    <div
-      onClick={onClick}
-      className="w-full bg-white dark:bg-gray-800 shadow-md rounded-lg p-6 mb-4 cursor-pointer hover:shadow-xl transition-shadow"
-    >
-      <h2 className="text-2xl font-bold mb-2">{title}</h2>
-      {description && <p className="text-gray-600 dark:text-gray-300">{description}</p>}
-    </div>
-  );
+    return(
+        <div
+        onClick={onClick}
+        className="w-full bg-gray-800 shadow-md rounded-lg p-6 mb-4 cursor-pointer hover:shadow-xl transition-shadow"
+        >
+            <h2 className="text-2xl font-bold mb-2">{title}</h2>
+            {description && <p className="text-gray-300"> {description}</p>}
+        </div>
+    );
 };
 
-export default FeatureCard;
+export default FeatureCard
